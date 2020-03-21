8c0c76bd71b1f4f7a1b290f9b89e32f0dfa0d41f
Daniel Schildt <daniel@autiomaa.org>
2020-03-21 18:19:36 +0200

    test: improve example test case
    - Improve example test case
      - Still not functional, but closer to it

--- pandemia-fi/src/App.test.js
+++ pandemia-fi/src/App.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test.skip("renders learn react link", () => {
+test.skip("renders Pandemia.fi link in the page", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Pandemia.fi/i);
   expect(linkElement).toBeInTheDocument();
 });
