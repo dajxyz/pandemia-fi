ba24af9a907cbbf72854cf00e936f0b2747c3297
Tomi Hiltunen <tomi@smartly.io>
2020-03-21 13:14:51 +0200

    Port the first Apex Charts implementation over from the old website

---
+++ pandemia-fi/src/pages/landing/components/nordics-graph/nordics-graph.tsx
@@ -0,0 +1,162 @@
+import React from "react";
+import Chart from "react-apexcharts";
+import timeSeriesData from "./time-series-data";
+
+const chartLabelFormatter = (y: number): string =>
+  `${(y / 50000).toFixed(4)}% väestöstä`;
+
+const chartOptions: object = {
+  chart: {
+    locales: [
+      {
+        name: "fi",
+        options: {
+          months: [
+            "Tammikuu",
+            "Helmikuu",
+            "Maaliskuu",
+            "Huhtikuu",
+            "Toukokuu",
+            "Kesäkuu",
+            "Heinäkuu",
+            "Elokuu",
+            "Syyskuu",
+            "Lokakuu",
+            "Marraskuu",
+            "Joulukuu"
+          ],
+          shortMonths: [
+            "Tammikuuta",
+            "Helmikuuta",
+            "Maaliskuuta",
+            "Huhtikuuta",
+            "Toukokuuta",
+            "Kesäkuuta",
+            "Heinäkuuta",
+            "Elokuuta",
+            "Syyskuuta",
+            "Lokakuuta",
+            "Marraskuuta",
+            "Joulukuuta"
+          ],
+          days: [
+            "Sunnuntai",
+            "Maanantai",
+            "Tiistai",
+            "Keskiviikko",
+            "Torstai",
+            "Perjantai",
+            "Lauantai"
+          ],
+          shortDays: ["Su", "Ma", "Ti", "Ke", "To", "Pe", "La"],
+          toolbar: {
+            exportToSVG: "Lataa SVG",
+            exportToPNG: "Lataa PNG",
+            exportToCSV: "Lataa CSV",
+            menu: "Valikko",
+            selection: "Valinta",
+            selectionZoom: "Valinnan zoomaus",
+            zoomIn: "Lähennä",
+            zoomOut: "Loitonna",
+            pan: "Panoroi",
+            reset: "Nollaa zoomaus"
+          }
+        }
+      }
+    ],
+    defaultLocale: "fi",
+    height: 380,
+    type: "line",
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: [3, 3, 5, 3, 3],
+    curve: "straight",
+    dashArray: [5, 5, 0, 5, 5]
+  },
+  colors: ["#CD0002", "#007CBD", "#02357F", "#F9CB0D", "#F02A2E"],
+  tooltip: {
+    enabled: true,
+    x: {
+      show: true,
+      format: "d.M.yyyy"
+    },
+    y: [
+      {
+        formatter: chartLabelFormatter
+      },
+      {
+        formatter: chartLabelFormatter
+      },
+      {
+        formatter: chartLabelFormatter
+      },
+      {
+        formatter: chartLabelFormatter
+      },
+      {
+        formatter: chartLabelFormatter
+      }
+    ]
+  },
+  title: {
+    text: "Vahvistetut tapaukset suhteutettuna väkilukuun",
+    align: "left"
+  },
+  subtitle: {
+    text: "Logaritminen asteikko",
+    align: "left"
+  },
+  xaxis: {
+    type: "datetime",
+    min: new Date("25 Feb 2020").getTime(),
+    labels: {
+      format: "d.M"
+    }
+  },
+  yaxis: {
+    logarithmic: "true",
+    opposite: true,
+    min: 2,
+    labels: {
+      formatter: (y: number) => `${(y / 50000).toFixed(4)}%`
+    }
+  },
+  legend: {
+    horizontalAlign: "center",
+    position: "bottom",
+    offsetY: 3,
+    itemMargin: {
+      horizontal: 10
+    }
+  },
+  grid: {
+    borderColor: "#f1f3fa"
+  },
+  responsive: [
+    {
+      breakpoint: 600,
+      options: {
+        chart: {
+          toolbar: {
+            show: !1
+          }
+        },
+        legend: {
+          show: !1
+        }
+      }
+    }
+  ]
+};
+
+const NordicsGraph = () => {
+  return <Chart options={chartOptions} series={timeSeriesData} type="line" />;
+};
+
+export default NordicsGraph;
--- pandemia-fi/src/pages/landing/landing.tsx
+++ pandemia-fi/src/pages/landing/landing.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Card from "../../components/card";
+import NordicsGraph from "./components/nordics-graph";
 import "./landing.css";
 
 const LandingPage: React.FunctionComponent = () => {
   return (
     <div className="landing-page__grid-container">
       <div className="landing-page__grid-item--large">
-        <Card>Card 1</Card>
+        <Card>
+          <NordicsGraph />
+        </Card>
       </div>
       <div className="landing-page__grid-item">
         <Card>Card 2</Card>
