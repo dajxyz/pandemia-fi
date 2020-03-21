462f7c39a4531c84bf76cad20e794e5f12b5f453
Tomi Hiltunen <tomi@smartly.io>
2020-03-21 11:39:21 +0200

    Use the layout component in main App

--- pandemia-fi/src/App.js
+++ pandemia-fi/src/App.js
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./components/layout";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <div className="App">wat</div>
+    </Layout>
   );
 }
 
