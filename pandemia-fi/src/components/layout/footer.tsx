43a864c5c8e33b7040da8a088d29217bce2c90de
Tomi Hiltunen <tomi@smartly.io>
2020-03-21 11:48:04 +0200

    Improve styling of the bootstrapped layout components

--- pandemia-fi/src/components/layout/footer.tsx
+++ pandemia-fi/src/components/layout/footer.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <footer className="layout__footer">Footer section placeholder</footer>;
+  return (
+    <footer className="layout__footer">-- Footer section placeholder --</footer>
+  );
 };
 
 export default Footer;
