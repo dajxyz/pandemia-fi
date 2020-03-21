59b7957cfa3773962fb1def415e39d2dfc175dfa
Tomi Hiltunen <tomi@smartly.io>
2020-03-21 22:12:10 +0200

    Implement a few placeholder routes

--- pandemia-fi/src/App.js
+++ pandemia-fi/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import LandingPage from "./pages/landing";
 
 function App() {
   return (
     <Layout>
-      <LandingPage />
+      <Router>
+        <Switch>
+          <Route path="/news">
+            <LandingPage />
+          </Route>
+          <Route path="/actions">
+            <LandingPage />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </Router>
     </Layout>
   );
 }
