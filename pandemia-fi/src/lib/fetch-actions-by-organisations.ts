d1e7cf0e150dd5241eed6e3dee077cfd5bb564c8
Tomi Hiltunen <tomi@smartly.io>
2020-03-22 10:55:49 +0200

    Add fetcher for actions by organisations

---
+++ pandemia-fi/src/lib/fetch-actions-by-organisations.ts
@@ -0,0 +1,24 @@
+interface ActionsByOrganisationsFromSource {
+  date: string;
+  organisation: string;
+  url: string;
+  organisationType: string;
+  location: string;
+}
+
+const buildNewsFeedItemFromSource = (
+  sourceItem: ActionsByOrganisationsFromSource
+): NewsFeedItem => ({
+  date: new Date(sourceItem.date),
+  url: sourceItem.url,
+  title: sourceItem.organisation,
+  additionalInfo: `${sourceItem.organisationType}, ${sourceItem.location}`
+});
+
+const fetchActionsByOrganisations = async (): Promise<NewsFeedItem[]> => {
+  const response = await fetch("/data-sources/organisaatioiden_toimet.json");
+  const results: ActionsByOrganisationsFromSource[] = await response.json();
+  return results.map(buildNewsFeedItemFromSource);
+};
+
+export default fetchActionsByOrganisations;
