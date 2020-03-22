40edb37d03e3e8bf304d8fbd03b2768c78fe6b3a
Alex Anisimov <alex.anisimov@wunder.io>
2020-03-22 10:36:02 +0200

    add fetching data from finnishCoronaData data source

--- pandemia-fi/src/App.js
+++ pandemia-fi/src/App.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "./components/layout";
 import LandingPage from "./pages/landing";
 
-function App() {
-  return (
-    <Layout>
-      <LandingPage />
-    </Layout>
-  );
-}
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      statData: {
+        numsConfirmed: 0,
+        numsConfirmedToday: 0,
+        numsDeaths: 0,
+        numsRecovered: 0,
+      },
+    };
+
+    this.today = new Date();
+    this.today.setDate(this.today.getDate());
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData');
+    const responseJson = await response.json();
+
+    this.setState({
+      statData: {
+        numsConfirmed: responseJson.confirmed.length,
+        numsConfirmedToday: responseJson.confirmed.filter(entry => {
+          const d = new Date(entry.date);
+          return d.getFullYear() === this.today.getFullYear()
+                 && d.getMonth() === this.today.getMonth()
+                 && d.getDate() === this.today.getDate();
+        }).length,
+        numsDeaths: responseJson.deaths.length,
+        numsRecovered: responseJson.recovered.length,
+      }
+    });
+  }
+
+
+  render() {
+
+    return (
+      <Layout>
+        <LandingPage
+          numsConfirmed={this.state.statData.numsConfirmed}
+          numsConfirmedToday={this.state.statData.numsConfirmedToday}
+          numsRecovered={this.state.statData.numsRecovered}
+          numsDeaths={this.state.statData.numsDeaths}
+        />
+      </Layout>
+    );
+  }
+};
 
 export default App;
--- pandemia-fi/src/pages/landing/landing.tsx
+++ pandemia-fi/src/pages/landing/landing.tsx
@@ -4,7 +4,15 @@ import HeroChartCard from "./components/hero-chart-card";
 import NumberMetricCard from "./components/number-metric-card";
 import "./landing.css";
 
-const LandingPage: React.FunctionComponent = () => {
+interface LandingPageProps {
+  numsConfirmed: number;
+  numsConfirmedToday: number;
+  numsDeaths: number;
+  numsRecovered: number;
+}
+
+const LandingPage = ({ numsConfirmed, numsConfirmedToday, numsRecovered, numsDeaths } : LandingPageProps) => {
+
   return (
     <div className="landing-page__grid-container">
       <div className="landing-page__grid-item--large">
@@ -14,12 +22,17 @@ const LandingPage: React.FunctionComponent = () => {
       </div>
       <div className="landing-page__grid-item">
         <Card>
-          <NumberMetricCard metric={450} explainer="Tartuntoja Suomessa" />
+          <NumberMetricCard metric={numsConfirmed} explainer="Tartuntoja Suomessa" />
         </Card>
       </div>
       <div className="landing-page__grid-item">
         <Card>
-          <NumberMetricCard metric={50} explainer="Uusia tartuntoja tänään" />
+          <NumberMetricCard metric={numsConfirmedToday} explainer="Uusia tartuntoja tänään" />
+        </Card>
+      </div>
+      <div className="landing-page__grid-item">
+        <Card>
+          <NumberMetricCard metric={numsRecovered} explainer="Toipuneet Suomessa" />
         </Card>
       </div>
       <div className="landing-page__grid-item">
