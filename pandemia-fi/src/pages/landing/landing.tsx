a930a28c3a40c36b3391578dca291ba88f81fb41
Tomi Hiltunen <tomi@smartly.io>
2020-03-22 12:54:03 +0200

    Fix duplicate React import

--- pandemia-fi/src/pages/landing/landing.tsx
+++ pandemia-fi/src/pages/landing/landing.tsx
@@ -1,7 +1,5 @@
-
-import React from "react";
-import { Flex, Box, Card, Heading } from 'rebass';
 import React, { useState, useEffect } from "react";
+import { Flex, Box, Card, Heading } from "rebass";
 import HeroChartCard from "./components/hero-chart-card";
 import NumberMetricCard from "./components/number-metric-card";
 import LinksCard from "./components/links-card";
@@ -13,7 +11,6 @@ interface CaseEntry {
 }
 
 function LandingPage() {
-
   const [numsConfirmed, setNumsConfirmed] = useState(0);
   const [numsConfirmedToday, setNumsConfirmedToday] = useState(0);
   const [numsRecovered, setNumsRecovered] = useState(0);
@@ -22,17 +19,21 @@ function LandingPage() {
   const today = new Date();
 
   useEffect(() => {
-    fetch('https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData')
+    fetch(
+      "https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData"
+    )
       .then(res => res.json())
       .then(data => {
         setNumsConfirmed(data.confirmed.length);
         setNumsConfirmedToday(
           data.confirmed.filter((entry: CaseEntry) => {
             const d = new Date(entry.date);
-            return d.getFullYear() === today.getFullYear()
-                   && d.getMonth() === today.getMonth()
-                   && d.getDate() === today.getDate();
-                 }).length
+            return (
+              d.getFullYear() === today.getFullYear() &&
+              d.getMonth() === today.getMonth() &&
+              d.getDate() === today.getDate()
+            );
+          }).length
         );
         setNumsRecovered(data.recovered.length);
         // setNumsDeaths(data.deaths.length);
@@ -40,21 +41,30 @@ function LandingPage() {
   });
 
   return (
-    <Flex flexWrap='wrap' mx={2}>
-      <Box px={2} width={[ '100%', '100%', '75%' ]}>
+    <Flex flexWrap="wrap" mx={2}>
+      <Box px={2} width={["100%", "100%", "75%"]}>
         <Card mb={3}>
           <HeroChartCard />
         </Card>
       </Box>
-      <Box px={2} width={[ '100%', '100%', '25%' ]}>
+      <Box px={2} width={["100%", "100%", "25%"]}>
         <Card mb={3}>
-          <NumberMetricCard metric={numsConfirmed} explainer="Tartuntoja Suomessa" />
+          <NumberMetricCard
+            metric={numsConfirmed}
+            explainer="Tartuntoja Suomessa"
+          />
         </Card>
         <Card mb={3}>
-          <NumberMetricCard metric={numsConfirmedToday} explainer="Uusia tartuntoja tänään" />
+          <NumberMetricCard
+            metric={numsConfirmedToday}
+            explainer="Uusia tartuntoja tänään"
+          />
         </Card>
         <Card mb={3}>
-          <NumberMetricCard metric={numsRecovered} explainer="Toipuneet Suomessa" />
+          <NumberMetricCard
+            metric={numsRecovered}
+            explainer="Toipuneet Suomessa"
+          />
         </Card>
         <Card mb={3}>
           <NumberMetricCard
@@ -63,18 +73,18 @@ function LandingPage() {
           />
         </Card>
       </Box>
-      <Box px={2} width={[ 1, 1, 2/3 ]}>
+      <Box px={2} width={[1, 1, 2 / 3]}>
         <Card>
           <Heading>Suomen luetuimmat korona-uutiset</Heading>
         </Card>
       </Box>
-      <Box px={2} width={[ 1, 1, 1/3 ]}>
+      <Box px={2} width={[1, 1, 1 / 3]}>
         <Card>
           <LinksCard />
         </Card>
       </Box>
     </Flex>
   );
-};
+}
 
 export default LandingPage;
