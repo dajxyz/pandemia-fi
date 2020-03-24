46fd395170b223c379ca2c0bc43df8cec2294d82
Tomi Hiltunen <tomi@smartly.io>
2020-03-24 19:53:04 +0200

    Add enum with available feed URLs

---
+++ pandemia-fi/src/lib/feeds-api.ts
@@ -0,0 +1,10 @@
+export enum Feeds {
+  // Viranomaistiedotteet 1
+  GovernmentalBulletins1 = "https://app.meltwater.com/gyda/outputs/5e70ed19dea75e201037047f/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss",
+  // Viranomaistiedotteet 2
+  GovernmentalBulletins2 = "https://app.meltwater.com/gyda/outputs/5e735eea1363521e1c685237/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss",
+  // Viranomaisten twiitit
+  GovernmentalTweets = "https://app.meltwater.com/gyda/outputs/5e71c8152ab0b4521c2db20c/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss",
+  // Paljon sosiaalisessa mediassa jaetut uutiset
+  PopularNews = "https://app.meltwater.com/gyda/outputs/5e70ed4d97e4a3b2ac4cbf0e/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss"
+}
