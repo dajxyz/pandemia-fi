44ccad44fda86869768d98b341ab626aef29d883
Tomi Hiltunen <tomi@smartly.io>
2020-03-24 22:13:54 +0200

    Add a helper for fetching, formatting and combining multiple Meltwater feeds

--- pandemia-fi/src/lib/feeds-api.ts
+++ pandemia-fi/src/lib/feeds-api.ts
@@ -1,10 +1,68 @@
-export enum Feeds {
-  // Viranomaistiedotteet 1
-  GovernmentalBulletins1 = "https://app.meltwater.com/gyda/outputs/5e70ed19dea75e201037047f/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss",
-  // Viranomaistiedotteet 2
-  GovernmentalBulletins2 = "https://app.meltwater.com/gyda/outputs/5e735eea1363521e1c685237/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss",
-  // Viranomaisten twiitit
-  GovernmentalTweets = "https://app.meltwater.com/gyda/outputs/5e71c8152ab0b4521c2db20c/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss",
-  // Paljon sosiaalisessa mediassa jaetut uutiset
-  PopularNews = "https://app.meltwater.com/gyda/outputs/5e70ed4d97e4a3b2ac4cbf0e/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss"
-}
+import Parser, { Output as RSSOutput, Item as RSSItem } from "rss-parser";
+import moment from "moment";
+
+const parser = new Parser();
+
+const feedHighlightColors: string[] = [
+  "#153084",
+  "#1B3FAB",
+  "#214ED3",
+  "#4169E1",
+  "#6988E7",
+  "#90A8EE",
+  "#B8C7F4"
+];
+
+const feedURLs: string[] = [
+  "https://app.meltwater.com/gyda/outputs/5e70ed19dea75e201037047f/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss",
+  "https://app.meltwater.com/gyda/outputs/5e735eea1363521e1c685237/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss",
+  "https://app.meltwater.com/gyda/outputs/5e71c8152ab0b4521c2db20c/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss",
+  "https://app.meltwater.com/gyda/outputs/5e70ed4d97e4a3b2ac4cbf0e/rendering?apiKey=5507cdbfa4b0adb412e15cf0&type=rss"
+];
+
+const buildFeedFromRawData = (
+  rawDataItem: RSSOutput,
+  assignedId: number
+): Feed => ({
+  id: assignedId,
+  color: feedHighlightColors[assignedId],
+  title: rawDataItem.title as string,
+  description: rawDataItem.description as string
+});
+
+const buildFeedItemsFromRawData = (
+  feed: Feed,
+  rawItems: RSSItem[]
+): FeedItem[] =>
+  rawItems.map((rawItem: RSSItem) => ({
+    feedId: feed.id,
+    color: feed.color,
+    title: rawItem.title as string,
+    link: rawItem.link as string,
+    description: rawItem.content as string,
+    creator: rawItem["dc:creator"],
+    dateTime: moment(rawItem.pubDate)
+  }));
+
+const fetchAllFeedsAndItems = async (): Promise<{
+  feeds: Feed[];
+  feedItems: FeedItem[];
+}> => {
+  const rawData: RSSOutput[] = await Promise.all(
+    feedURLs.map(feedURL => parser.parseURL(feedURL))
+  );
+  const feeds: Feed[] = rawData.map(buildFeedFromRawData);
+  const feedItems: FeedItem[] = feeds
+    .map((feed: Feed) =>
+      buildFeedItemsFromRawData(feed, rawData[feed.id].items ?? [])
+    )
+    .flat()
+    .sort((a: FeedItem, b: FeedItem) => a.dateTime.diff(b.dateTime));
+
+  return {
+    feeds,
+    feedItems
+  };
+};
+
+export default fetchAllFeedsAndItems;
