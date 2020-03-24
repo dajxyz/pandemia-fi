c7bb99df1c05bd2eabee5c5fcf3b8035cf34c1ef
Tomi Hiltunen <tomi@smartly.io>
2020-03-24 22:13:26 +0200

    Add interfaces for new Feed and FeedItem types

--- pandemia-fi/src/types.d.ts
+++ pandemia-fi/src/types.d.ts
@@ -6,3 +6,20 @@ interface NewsFeedItem {
   title: string;
   additionalInfo: string;
 }
+
+interface Feed {
+  id: number;
+  color: string;
+  title: string;
+  description: string;
+}
+
+interface FeedItem {
+  feedId: number;
+  color: string;
+  title: string;
+  description: string;
+  link: string;
+  creator: string;
+  dateTime: import("moment").Moment;
+}
