c58271b74554786656a1f665b8a345d432e85ac0
Tomi Hiltunen <tomi@smartly.io>
2020-03-26 14:54:26 +0200

    Get rid of cusotm CSS on number metrics card

--- pandemia-fi/src/pages/landing/components/number-metric-card/number-metric-card.tsx
+++ pandemia-fi/src/pages/landing/components/number-metric-card/number-metric-card.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Box, Text } from "rebass";
-import "./number-metric-card.css";
 
 interface NumberMetricCardProps {
   metric: number;
@@ -8,29 +7,17 @@ interface NumberMetricCardProps {
 }
 
 const numberFormatter = new Intl.NumberFormat("fi-FI", {
-  maximumSignificantDigits: 3
+  maximumSignificantDigits: 3,
 });
 
 const NumberMetricCard = ({ metric, explainer }: NumberMetricCardProps) => {
   return (
-    <Box
-      className="number-metric-card"
-    >
-      <Text
-        className="number-metric-card__metric"
-        textAlign="center"
-        fontSize={[5, 6]}
-        color="text"
-      >
+    <Box>
+      <Text textAlign="center" fontSize={[5, 6]} color="text">
         {numberFormatter.format(metric)}
       </Text>
 
-      <Text
-        className="number-metric-card__explainer"
-        textAlign="center"
-        fontSize={[2, 2, 2]}
-        color="text"
-      >
+      <Text textAlign="center" fontSize={[2, 2, 2]} color="text">
         {explainer}
       </Text>
     </Box>
