8a0e761dc134678267a4970270fb23ff58f5b1d2
Daniel Schildt <daniel@autiomaa.org>
2020-04-07 03:04:45 +0300

    fix: remove unused import from App component
    - Remove unused import from App component

--- pandemia-fi/src/App.js
+++ pandemia-fi/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import { Message } from "theme-ui";
 
 import Layout from "./components/layout";
 import LandingPage from "./pages/landing";
