3ed787cf32a6e9e1ef0a46a00cfa0bd980f4b8d3
Daniel Schildt <daniel@autiomaa.org>
2020-04-10 20:52:36 +0300

    fix: improve NewsFeedItem layout & code structure
    - Improve NewsFeedItem layout & code structure
    - Convert the code to use Flex component from Rebass
    - Add minimum width to the date & time area
    - Change text color of the `NewsFeedItem__date` element

--- pandemia-fi/src/pages/uutishuone/components/news-feed-item/news-feed-item.tsx
+++ pandemia-fi/src/pages/uutishuone/components/news-feed-item/news-feed-item.tsx
@@ -18,49 +18,54 @@ const NewsFeedItem: React.FunctionComponent<NewsFeedItemProps> = ({
       target="_blank"
       rel="noreferrer noopener"
     >
-      <Box
+      <Flex
         sx={{
           p: 2,
           borderBottom: "1pt dashed #000",
           backgroundColor: "#fff",
+          maxWidth: "100%",
         }}
       >
-        <Flex>
-          <Box>
-            <Heading
-              className="NewsFeedItem__title"
-              fontSize={[2, 2, 2]}
-              sx={{
-                color: "#1e4454",
-              }}
-            >
-              {feedItem.title}
-            </Heading>
-            <Text
-              className="NewsFeedItem__description"
-              fontSize={[1, 1, 2]}
-              sx={{
-                lineHeight: "18px",
-                color: "#1e4454",
-              }}
-            >
-              {feedItem.description}
-            </Text>
-          </Box>
-          <Box>
-            <Text
-              className="NewsFeedItem__date"
-              fontSize={[1, 1, 2]}
-              sx={{
-                color: "gray",
-                textAlign: "right",
-              }}
-            >
-              {feedItem.dateTime.format("D.M.YYYY HH:mm:ss")}
-            </Text>
-          </Box>
-        </Flex>
-      </Box>
+        <Box>
+          <Heading
+            className="NewsFeedItem__title"
+            fontSize={[2, 2, 2]}
+            sx={{
+              color: "#1e4454",
+            }}
+          >
+            {feedItem.title}
+          </Heading>
+          <Text
+            className="NewsFeedItem__description"
+            fontSize={[1, 1, 2]}
+            sx={{
+              lineHeight: "18px",
+              color: "#1e4454",
+            }}
+          >
+            {feedItem.description}
+          </Text>
+        </Box>
+        <Box
+          sx={{
+            px: 1,
+            backgroundColor: "#fff",
+            minWidth: "10ex",
+          }}
+        >
+          <Text
+            className="NewsFeedItem__date"
+            fontSize={[1, 1, 2]}
+            sx={{
+              color: "#3A6579",
+              textAlign: "right",
+            }}
+          >
+            {feedItem.dateTime.format("D.M.YYYY HH:mm:ss")}
+          </Text>
+        </Box>
+      </Flex>
     </Link>
   );
 };
